0c6067b6947140f8b3b565a0dd399d7130e00240
t-yamakoshi <takyamakoshi@puc.pu-toyama.ac.jp>
2023-08-23 14:51:20 +0900

    init commit

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -1,10 +1,40 @@
 "use client"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Pie } from "react-chartjs-2"
+import { Chart as ChartJS, CategoryScale, ArcElement, BarElement, LinearScale, Title, Tooltip, Legend } from 'chart.js'
+import { Pie, Bar } from "react-chartjs-2"
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, Title, BarElement)
 
 export default function ChartPage() {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top" as const
+            },
+            title: {
+                display: true,
+                text: "Chart.js Bar Chart"
+            },
+        },
+    }
+    const labels = ["Person1", "Person2", "Person3"]
+
+    const data2 = {
+        labels,
+        datasets: [
+            {
+                label: "Dataset 1",
+                data: [12, 19, 3, 5, 2, 3, 15],
+                backgroundColor: "rgba(255, 99, 132, 0.5)"
+            },
+            {
+                label: "Dataset 2",
+                data: [2, 29, 5, 5, 2, 3, 10],
+                backgroundColor: "rgba(54, 162, 235, 0.5)"
+            },
+        ],
+    }
+
     const data = {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [
@@ -32,9 +62,43 @@ export default function ChartPage() {
         ],
     };
 
+    // const options = {
+    //     responsive: true,
+    //     plugins: {
+    //         legend: {
+    //             position: "top"
+    //         },
+    //         title: {
+    //             display: true,
+    //             text: "Chart.js Bar Chart"
+    //         }
+    //     },
+    //     maintainAspectRatio: false,
+    // };
+
+    // const Bardata = {
+    //     labels, // x軸のラベルの配列
+    //     datasets: [
+    //         {
+    //             label: "Dataset 1", // 凡例
+    //             data: data1,        // データの配列(labelsと要素数同じ)
+    //             backgroundColor: "rgba(255, 99, 132, 0.5)" // グラフの棒の色
+    //         },
+    //         {
+    //             label: "Dataset 2",
+    //             data: data2,
+    //             backgroundColor: "rgba(53, 162, 235, 0.5)"
+    //         }
+    //     ]
+    // };
+
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Pie data={data} options={{ responsive: true, maintainAspectRatio: false }} />
+            {/* <Pie data={data} options={{ responsive: true, maintainAspectRatio: false }} /> */}
+            {/* <Bar data={Bardata} options={options}/> */}
+            <div className="">
+                <Bar className='w-100' options={options} data={data2} />
+            </div>
         </div>
     )
 }
