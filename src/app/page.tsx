c7d95d4a5a5df16c3a3feaa0aee8c271cc4739e9
t-yamakoshi <takyamakoshi@puc.pu-toyama.ac.jp>
2023-09-07 15:19:55 +0900

    tmp commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import { storage } from 
 
 export default function Home() {
   return (
-    <div className="">
-      <Link href="/chart"><button className="btn btn-success">グラフ画面に移動する</button></Link>
+    // <div className="">
+    //   <Link href="/chart"><button className="btn btn-success">グラフ画面に移動する</button></Link>
+    // </div>
+    <div className="h-screen w-screen flex justify-center items-center">
+      <input name="file" type='file' className="file-input file-input-bordered file-input-primary w-full max-w-xs" accept='videos/*'></input><br />
+      <input type="button" value="Submit" className="btn btn-outline btn-primary"></input> 
     </div>
   )
 }
